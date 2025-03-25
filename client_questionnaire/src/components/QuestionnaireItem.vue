cdfa6407738d5f1d88953ba4446ff9b5186899a7
Saunier Leo <o22300209@or-iut-i203-f30.iut45.univ-orleans.fr>
2025-03-25 18:02:20 +0100

    Ajout QuestionnaireItem

---
+++ client_questionnaire/src/components/QuestionnaireItem.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    props: {
+        questionnaire: Object
+    },
+    methods: {
+        selectQuestionnaire() {
+            this.$emit('selectQuestionnaire', this.questionnaire);
+        },
+        deleteQuestionnaire() {
+            this.$emit('deleteQuestionnaire', this.questionnaire);
+        }
+    },
+    emits: ['selectQuestionnaire', 'deleteQuestionnaire']
+};
+</script>
+
+
+<template>
+    <div class="card">
+        <h3>{{ questionnaire.title }}</h3>
+        <p>{{ questionnaire.description }}</p>
+        <button @click="selectQuestionnaire">Select</button>
+        <button @click="deleteQuestionnaire">Delete</button>
+    </div>
+</template>
