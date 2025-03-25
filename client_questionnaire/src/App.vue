fdf98aef0bb794817615d016709de315d54f0e38
YohannVilcoq <ryrycantonais@gmail.com>
2025-03-25 18:13:42 +0100

    adaptation au questionnaireItem

--- client_questionnaire/src/App.vue
+++ client_questionnaire/src/App.vue
@@ -3,24 +3,27 @@ import QuestionnaireItem from './components/QuestionnaireItem.vue';
 </script>
 
 <template>
-    <link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-  integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-  crossorigin="anonymous"/>
-  <div>
-    <h1>Gestion de questionnaires</h1>
-  </div>
-  <div>
-    <h2>Questionnaires</h2>
-    <ol>
-      <QuestionnaireItem
-        v-for="questionnaire in questionnaires">
-      </QuestionnaireItem>
-    </ol>
-  </div>
-  <div class="input-group">
- 
-  </div>
+  <link
+rel="stylesheet"
+href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+crossorigin="anonymous"/>
+<div>
+  <h1>Gestion de questionnaires</h1>
+</div>
+<div>
+  <h2>Questionnaires</h2>
+  <ol>
+    <QuestionnaireItem
+      v-for="questionnaire in questionnaires"
+      :questionnaire="questionnaire"
+      @selectQuestionnaire="selectQuestionnaire"
+      @deleteQuestionnaire="deleteQuestionnaire">
+    </QuestionnaireItem>
+  </ol>
+</div>
+<div class="input-group">
+
+</div>
 </template>
 
