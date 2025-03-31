a81bb877bbb60cadfd2c916b6c13fc9dd4226176
YohannVilcoq <ryrycantonais@gmail.com>
2025-03-31 08:16:05 +0200

    init de l'item des questions

---
+++ client_questionnaire/src/components/QuestionItem.vue
@@ -0,0 +1,35 @@
+<script>
+
+export default {
+    props : {
+        questions: Object
+    },
+    methods: {
+        addQuestionnaire() {
+            console.log("addQuestionnaire");
+        }
+    },
+    emits : ["addQuestionnaire"]
+}
+
+const selected = ref(false);
+</script>
+
+<template>
+    <div>
+        <div v-if="selected">
+            <div v-for="question in questionnaire.questions"
+                :question="question"
+                @deleteQuestion="deleteQuestion">
+            
+            </div>
+            <label>
+                <input type="text" v-model="questionnaire.title" placeholder="Nom du questionnaire">
+                <input type="text" v-model="questionnaire.description" placeholder="Description du questionnaire">
+            </label>
+            <button class="btn btn-add" @click="addQuestionnaire">Ajouter</button>
+        </div>
+    </div>
+
+
+</template>
